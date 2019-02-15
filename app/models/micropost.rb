15a4532cde9ca0082b05761f54e3dffa529dac49
Will Coulter <will.coulter01@stclairconnect.ca>
2019-02-15 02:02:57 -0500

    Finished

---
+++ app/models/micropost.rb
@@ -0,0 +1,5 @@
+class Micropost < ApplicationRecord
+  validates :message, presence: true
+
+  belongs_to :user
+end
---
+++ app/models/user.rb
@@ -0,0 +1,7 @@
+class User < ApplicationRecord
+  validates :first_name, presence:true
+  validates :last_name,presence: true
+  validates :email, presence: true
+
+  has_many :microposts
+end
